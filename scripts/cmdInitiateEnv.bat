90610d4a626c98887494bb82556d85f4f440fd90
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2024-02-24 18:53:16 +0100

    EDIT: post-precommit changes

--- scripts/cmdInitiateEnv.bat
+++ scripts/cmdInitiateEnv.bat
@@ -3,15 +3,16 @@ cd ..
 setlocal
 :PROMPT
 SET AREYOUSURE=N
-SET /P AREYOUSURE=Do you want to create new virtual environment (Y/[N])? 
+SET /P AREYOUSURE=Do you want to create new virtual environment (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 SET PATH=C:\Compilers\Python311\Scripts\;C:\Compilers\Python311\;%PATH%
 python -m venv .venv
 call .\.venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r requirements-dev.txt
+pip install uv
+uv pip install -r requirements.txt
+uv pip install -r requirements-dev.txt
 
 :END
 endlocal
